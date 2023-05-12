16b122344791760cbdd9f3390626d0af81b82a53
Nicole <nicole.frigo.1@studenti.unipd.it>
2023-05-12 16:43:43 +0200

    aggiunte shared preferences per la pagina profilo

--- lib/screens/EditProfilePage.dart
+++ lib/screens/EditProfilePage.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 class EditprofilePage extends StatelessWidget {
    EditprofilePage({Key? key}) : super(key: key);
 
   static const routename = 'Edit Profile';
+  
+  final TextEditingController usernameController=TextEditingController();
+  final TextEditingController passwordController=TextEditingController();
   final TextEditingController ageController=TextEditingController();
 
-  final _formKeyUsername = GlobalKey<FormState>();
-  final _formKeyAgePassword = GlobalKey<FormState>();
-  final _formKeyAge = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
+
+  Future<void> saveUsername(String username) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('username', username); //save string ('username'=key, username= value)
+  }
+
+  Future<void> savePassword(String password) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('password', password);
+  }
+
+  Future<void> saveAge(int age) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('age', age);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,29 +36,37 @@ class EditprofilePage extends StatelessWidget {
         actions: <Widget>[
           IconButton(
           onPressed: ()async{
-            if(_formKeyAge.currentState!.validate()){
+            if(_formKey.currentState!.validate() ){
+              await saveUsername(usernameController.text); // Salva il valore dello username
+              await savePassword(passwordController.text); // Salva il valore della password
+              await saveAge(int.parse(ageController.text)); // Salva il valore dell'età come intero
               Navigator.pop(context);
-            }//if
-            ScaffoldMessenger.of(context).showSnackBar(
+
+              ScaffoldMessenger.of(context).showSnackBar(
               const SnackBar(content: Text('Your edits has been saved'),
               backgroundColor: Colors.grey,
             ),
             ); //ScaffoldMessenger
+            }//if
+            
           },
           icon: const Icon(Icons.check_rounded),
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal:30,vertical:30),
-        child: ListView(
-          children: <Widget>[
-            usernameTextField(),
-            const SizedBox(height:20),
-            passwordTextField(),
-            const SizedBox(height:20),
-            ageTextField(),
-          ],
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal:30,vertical:30),
+          child: ListView(
+            children: <Widget>[
+              usernameTextField(),
+              const SizedBox(height:20),
+              passwordTextField(),
+              const SizedBox(height:20),
+              ageTextField(),
+            ],
+          ),
         ),
       ),
     );
@@ -51,84 +78,89 @@ class EditprofilePage extends StatelessWidget {
 //widget -------------------------------------------------------------
 
 Widget usernameTextField(){
-  return Form(
-    key: _formKeyUsername,
-    child: TextFormField(
-    validator: (value) {
-    if (value == null || value.isEmpty) {
-      return 'Please enter your age';
-    } else if (int.tryParse(value) == null) {
-      return 'Please enter an integer valid number';
-    }
-    return null;
+  return TextFormField(
+  validator: (value) {
+  if (value == null || value.isEmpty) {
+    return 'Please enter your Username';
+  }
+  return null;
   },
-  controller: ageController,
-  enabled: true,
-    decoration:  InputDecoration(
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(90.0)
-      ),
-      focusedBorder:  OutlineInputBorder(
-          borderSide: const BorderSide(
-              width: 1,
-              color: Colors.lightGreen ),
-          borderRadius: BorderRadius.circular(90.0),
-              ),
-      labelText: 'Username',
-      labelStyle: const TextStyle(color:Colors.lightGreen),
-      ),
-    ),
-  );
-}
-
-Widget passwordTextField(){
-  return Form(
-    key: _formKeyAgePassword,
-    child: TextFormField(
-    decoration:  InputDecoration(
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(90.0)
-      ),
-      focusedBorder:  OutlineInputBorder(
-          borderSide: const BorderSide(
-              width: 1,
-              color: Colors.lightGreen ),
-          borderRadius: BorderRadius.circular(90.0),
-              ),
-      labelText: 'Password',
-      labelStyle: const TextStyle(color:Colors.lightGreen),
-      ),
-    ),
-  );
-}
-Widget ageTextField(){
-return Form(
-  key: _formKeyAge,
-  child: TextFormField(
-    validator: (value) {
-    if (value == null || value.isEmpty) {
-      return 'Please enter your age';
-    } else if (int.tryParse(value) == null) {
-      return 'Please enter an integer valid number';
-    }
-    return null;
-  },
-  controller: ageController,
+  controller: usernameController,
   enabled: true,
   decoration:  InputDecoration(
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(90.0)
-      ),
-      focusedBorder:  OutlineInputBorder(
-          borderSide: const BorderSide(
-              width: 1,
-              color: Colors.lightGreen ),
-          borderRadius: BorderRadius.circular(90.0),
-              ),
-      labelText: 'Age',
-      labelStyle: const TextStyle(color:Colors.lightGreen),
-      ),
-  ),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(90.0)
+    ),
+    focusedBorder:  OutlineInputBorder(
+        borderSide: const BorderSide(
+            width: 1,
+            color: Colors.lightGreen ),
+        borderRadius: BorderRadius.circular(90.0),
+            ),
+    labelText: 'Username',
+    hintText: 'Marzia',
+    labelStyle: const TextStyle(color:Colors.lightGreen),
+    ),
+  );
+} //usernameTextField
+
+
+Widget passwordTextField(){
+  return TextFormField(
+  validator: (value) {
+  if (value == null || value.isEmpty) {
+    return 'Please enter your Password';
+  }
+  return null;
+  },
+  controller: passwordController,
+  enabled: true,
+  decoration:  InputDecoration(
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(90.0)
+    ),
+    focusedBorder:  OutlineInputBorder(
+        borderSide: const BorderSide(
+            width: 1,
+            color: Colors.lightGreen ),
+        borderRadius: BorderRadius.circular(90.0),
+            ),
+    labelText: 'Password',
+    hintText: '123456',
+    labelStyle: const TextStyle(color:Colors.lightGreen),
+    ),
+    obscureText: true,
+  );
+} // passwordTextField
+
+
+
+Widget ageTextField(){
+return TextFormField(
+  validator: (value) {
+  if (value == null || value.isEmpty) {
+    return 'Please enter your age';
+  } else if (int.tryParse(value) == null) {
+    return 'Please enter an integer valid number';
+  }
+  return null;
+},
+controller: ageController,
+enabled: true,
+decoration:  InputDecoration(
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(90.0)
+    ),
+    focusedBorder:  OutlineInputBorder(
+        borderSide: const BorderSide(
+            width: 1,
+            color: Colors.lightGreen ),
+        borderRadius: BorderRadius.circular(90.0),
+            ),
+    labelText: 'Age',
+    hintText: '23',
+    labelStyle: const TextStyle(color:Colors.lightGreen),
+    ),
 );
 
 }// ageTextField
--- lib/screens/ProfilePage.dart
+++ lib/screens/ProfilePage.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:progetto_wearable/screens/EditProfilePage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 class ProfilePage extends StatefulWidget {
   ProfilePage({Key? key}) : super(key: key);
@@ -14,8 +16,12 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   static const routename = 'Profile';
+  
+  String? username;
+  String? password;
+  int? age;
 
-  var _image;
+  /*var _image;
   var imagePicker;
   var type;
 
@@ -23,64 +29,156 @@ class _ProfilePageState extends State<ProfilePage> {
   void initState() {
     super.initState();
     imagePicker = ImagePicker();
+  }*/
+
+  @override
+  void initState() {
+    super.initState();
+    loadSavedValues();
+  }
+
+  Future<void> loadSavedValues() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      username = prefs.getString('username');
+      password = prefs.getString('password');
+      age = prefs.getInt('age');
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('$routename')),
-      body: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 52,
-          ),
-          Center(
-            child: GestureDetector(
-              onTap: () async {
-                var source = ImageSource.camera;
-
-                XFile image = await imagePicker.pickImage(
-                    source: source,
-                    imageQuality: 50,
-                    preferredCameraDevice: CameraDevice.front);
-                setState(() {
-                  _image = File(image.path);
-                });
-              },
-              child: Container(
-                width: 200,
-                height: 200,
-                decoration: BoxDecoration(color: Colors.red[200]),
-                child: _image != null
-                    ? CircleAvatar(
-                      radius: 90.0,
-                      
-                      child: Image.file(
-                          _image,
-                          width: 200.0,
-                          height: 200.0,
-                          fit: BoxFit.fill,
-                        ),
-                    )
-                    : Container(
-                        decoration: BoxDecoration(color: Colors.red[200]),
-                        width: 200,
-                        height: 200,
-                        child: Icon(
-                          Icons.camera_alt,
-                          color: Colors.grey[800],
-                        ),
-                      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              /*SizedBox(
+                height: 52,
               ),
-            ),
-          ),
-          ElevatedButton(onPressed: (){
-            Navigator.push(context, MaterialPageRoute(fullscreenDialog: true, builder: (context)=> EditprofilePage()));
-            }, 
-            child:  Text("Edit Profile"),
-          ),
+              Center(
+                child: GestureDetector(
+                  onTap: () async {
+                    var source = ImageSource.camera;
+          
+                    XFile image = await imagePicker.pickImage(
+                        source: source,
+                        imageQuality: 50,
+                        preferredCameraDevice: CameraDevice.front);
+                    setState(() {
+                      _image = File(image.path);
+                    });
+                  },
+                  child: Container(
+                    width: 200,
+                    height: 200,
+                    decoration: BoxDecoration(color: Colors.red[200]),
+                    child: _image != null
+                        ? CircleAvatar(
+                          radius: 90.0,
+                          
+                          child: Image.file(
+                              _image,
+                              width: 200.0,
+                              height: 200.0,
+                              fit: BoxFit.fill,
+                            ),
+                        )
+                        : Container(
+                            decoration: BoxDecoration(color: Colors.red[200]),
+                            width: 200,
+                            height: 200,
+                            child: Icon(
+                              Icons.camera_alt,
+                              color: Colors.grey[800],
+                            ),
+                          ),
+                  ),
+                ),
+              ),*/
+              Text("Username",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 24 )),
+              Text("$username",style: TextStyle(fontSize: 20, color: Colors.grey)),
+              SizedBox(height: 20),
+              imageProfile(),
+              SizedBox(height: 20),
+              Text("Email",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17 )),
+              Text("marzia@gmail.com",style: TextStyle(color:Colors.grey)), //email da implementare
+              SizedBox(height: 5),
+              Text("Age",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17 )),
+              Text("$age",style: TextStyle(color:Colors.grey)),
+              SizedBox(height: 5),
+              Text("Level",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17 )),
+              Text("1",style: TextStyle(color:Colors.grey)), //da implementare
+              SizedBox(height: 5),
+              Text('Password: $password'), // da togliere, solo per far vedere che va
+               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children:[
+                  IconButton(
+                    // iconSize: 72,
+                    onPressed: (){}, 
+                    icon: const Icon(Icons.auto_awesome)),
+                const Text("Your reward",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17 )),
         ],
+        ),
+          
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                fullscreenDialog: true,
+                builder: (context) => EditprofilePage(),
+              ),
+            ).then((value) {
+              // Aggiorna i dati quando si torna da Edit Profile
+              loadSavedValues();
+            });
+          },
+          child: const Text("Edit Profile"),
+        ),
+        ],
+        ),
+        ),
       ),
     );
   }
-}
+
+  
+  Widget imageProfile(){
+  return Stack(
+    children: <Widget>[
+      CircleAvatar(
+        radius: 80.0,
+        backgroundImage: AssetImage("assets/images/user.png")
+
+        ),
+      
+      Positioned(
+        bottom: 20.0,
+        right: 20.0,
+        child: InkWell(
+          //usato InkWell perchè possiede il metodo onTap()
+          onTap: (){
+            
+          },
+          child: const Icon(
+            Icons.camera_alt, 
+            color: Colors.white,
+            size: 28.0)
+        ),
+        ),
+    ],
+  );
+} //imageProfile
+
+
+} // _ProfilePageState
+
