1cc31caacf7936082899c0e4862911e6546de03a
mariobon <mario.semprebon99@gmail.com>
2023-05-29 23:47:48 +0200

    aggiunto load dati da IMPACT SERVER

--- lib/screens/AdvicePage.dart
+++ lib/screens/AdvicePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../services/impactService.dart';
 
 class AdvisePage extends StatelessWidget {
   const AdvisePage({Key? key}) : super(key: key);
@@ -7,9 +9,102 @@ class AdvisePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //print('${CalendarPage.routename} built');
-    return const Center(
-      child: Text('Vari consigli...'),
-    );
+    final tokenManager = TokenManager();
+
+    return Center(
+        child: Column(
+      children: [
+        ElevatedButton(
+          onPressed: () async {
+            final sp = await SharedPreferences.getInstance();
+            final result = await tokenManager.requestData();
+            double somma = 0;
+            double totalValidActivities = 0;
+            for (var i = 0; i < result!.length; i++) {
+              if (result[i].finalDistance > 0) {
+                somma += result[i].finalDistance;
+                totalValidActivities += 1;
+              }
+            }
+            var meanDistance = somma / totalValidActivities;
+
+            sp.setDouble('distance', meanDistance);
+
+            final message =
+                'Distanza media: ${meanDistance.toStringAsFixed(2)} km';
+
+            ScaffoldMessenger.of(context)
+              ..removeCurrentSnackBar()
+              ..showSnackBar(SnackBar(content: Text(message)));
+          },
+          child: Text('get_data'),
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            final result = await tokenManager.isBackendUp();
+            final message =
+                result ? 'IMPACT backend is up!' : 'IMPACT backend is down!';
+            ScaffoldMessenger.of(context)
+              ..removeCurrentSnackBar()
+              ..showSnackBar(SnackBar(content: Text(message)));
+          },
+          child: Text('DEBUG:Ping IMPACT'),
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            final result = await tokenManager.getAndStoreTokens();
+            final message =
+                result == 200 ? 'Request successful' : 'Request failed';
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text('Get tokens'),
+                    content: Text(message),
+                    actions: [
+                      TextButton(
+                        child: Text('Close'),
+                        onPressed: () {
+                          Navigator.of(context).pop(); // Close the alert dialog
+                        },
+                      ),
+                    ],
+                  );
+                });
+          },
+          child: Text('DEBUG:Get tokens'),
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            final sp = await SharedPreferences.getInstance();
+            final refresh = sp.getString('refresh');
+            final message;
+            if (refresh == null) {
+              message = 'No stored tokens';
+            } else {
+              final result = await tokenManager.refreshTokens();
+              message = result == 200 ? 'Request successful' : 'Request failed';
+            } //if-else
+            ScaffoldMessenger.of(context)
+              ..removeCurrentSnackBar()
+              ..showSnackBar(SnackBar(content: Text(message)));
+          },
+          child: Text('DEBUG:Refresh tokens'),
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            final access = await tokenManager.getAccessToken();
+            final refresh = await tokenManager.getRefreshToken();
+            final message = access == null
+                ? 'No stored tokens'
+                : 'access: $access; refresh: $refresh';
+            ScaffoldMessenger.of(context)
+              ..removeCurrentSnackBar()
+              ..showSnackBar(SnackBar(content: Text(message)));
+          },
+          child: Text('DEBUG:Print tokens'),
+        )
+      ],
+    ));
   } //build
 } //ProfilePage
--- lib/screens/ComunityPage.dart
+++ lib/screens/ComunityPage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icon.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:progetto_wearable/utils/palette.dart';
 
 class CommunityPage extends StatelessWidget {
   const CommunityPage({Key? key}) : super(key: key);
@@ -7,6 +10,25 @@ class CommunityPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Fare qualcosa con Community(?)'));
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) => Card(
+        elevation: 5,
+        child: Dismissible(
+          key: UniqueKey(),
+          background: Container(color: Palette.tertiaryColor),
+          secondaryBackground: Container(color: Palette.black),
+          child: ListTile(
+            title: Text('${index + 1}'),
+            trailing: LineIcon(LineIcons.alternateArrowCircleUp),
+            iconColor: index % 2 == 0 ? Colors.red : Colors.green,
+            tileColor: index % 2 == 0
+                ? Palette.mainColor
+                : Color.fromARGB(255, 214, 214, 214),
+          ),
+          onDismissed: (direction) {},
+        ),
+      ),
+    );
   } //buil
 } //ProfilePage
