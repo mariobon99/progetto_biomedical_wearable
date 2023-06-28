c396434bf0649f491bb76a33a2f3dec2ae7403ac
Nicole <nicole.frigo.1@studenti.unipd.it>
2023-06-28 17:30:13 +0200

    Livello ricavato dal database

--- lib/screens/ProfilePage.dart
+++ lib/screens/ProfilePage.dart
@@ -29,7 +29,7 @@ class _ProfilePageState extends State<ProfilePage> {
   File? _image;
   var imagePicker;
 
-  int Userlevel = 3;
+  int Userlevel = 1;
 
   @override
   void initState() {
@@ -52,147 +52,164 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Palette.bgColor,
-      appBar: AppBar(
-        title: const Text('$routename'),
-        actions: <Widget>[
-            IconButton(
-              onPressed: (){
-                openDialog(context).then((value) {
-                    // Aggiorna i dati quando si torna da Edit Profile
-                    loadSavedValues();
-                  });
-              },
-              icon: const Icon(Icons.edit))
-        ],
-        ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-          child: Column(
-            children: [
-              Stack(
-                alignment: Alignment.topCenter,
-                children: <Widget>[
-                  Container(
-                    margin: const EdgeInsets.only(top: 90.0),
-                    width: double.infinity,
-                    child: Card(
-                      color: Palette.mainColorShade,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.only(
-                                top: 70, bottom: 0, left: 10, right: 10),
-                            child: Column(
-                              children: <Widget>[
-                                const Text("Username",
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold)),
-                                Text("$username"),
-                                const SizedBox(height: 20),
-                                const Text("Email",
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold)),
-                                Text("$email"),
-                                const SizedBox(height: 20),
-                              ],
+    return Consumer<DatabaseRepository>(builder: (context, dbr, child) {
+      return FutureBuilder(
+          future: dbr.findUserLevel(0),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              Userlevel = snapshot.data as int;
+              return Scaffold(
+                backgroundColor: Palette.bgColor,
+                appBar: AppBar(
+                  title: const Text('$routename'),
+                  actions: <Widget>[
+                      IconButton(
+                        onPressed: (){
+                          openDialog(context).then((value) {
+                              // Aggiorna i dati quando si torna da Edit Profile
+                              loadSavedValues();
+                            });
+                        },
+                        icon: const Icon(Icons.edit))
+                  ],
+                  ),
+                body: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: Column(
+                      children: [
+                        Stack(
+                          alignment: Alignment.topCenter,
+                          children: <Widget>[
+                            Container(
+                              margin: const EdgeInsets.only(top: 90.0),
+                              width: double.infinity,
+                              child: Card(
+                                color: Palette.mainColorShade,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          top: 70, bottom: 0, left: 10, right: 10),
+                                      child: Column(
+                                        children: <Widget>[
+                                          const Text("Username",
+                                              style:
+                                                  TextStyle(fontWeight: FontWeight.bold)),
+                                          Text("$username"),
+                                          const SizedBox(height: 20),
+                                          const Text("Email",
+                                              style:
+                                                  TextStyle(fontWeight: FontWeight.bold)),
+                                          Text("$email"),
+                                          const SizedBox(height: 20),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
                             ),
+                            _imageProfile(),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        ListTile(
+                          title: const Text('Your Level'),
+                          subtitle: const Text('Level name'),
+                          leading: const ImageIcon(
+                            AssetImage("assets/images/level-up.png"),
+                            color: Palette.black,
+                            size: 30,
                           ),
-                        ],
-                      ),
+                          trailing: Icon(Icons.arrow_forward),
+                          tileColor: Palette.mainColorShade,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          onTap: (){
+                            if (Userlevel==1){
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return CustomAlert(
+                                    title: Levels.title1,
+                                    imagePath: Levels.imagePath1,
+                                    description: Levels.description1,
+                                  );
+                                },
+                              );
+                            } 
+                            if (Userlevel==2){
+                              showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return CustomAlert(
+                                  title: Levels.title2,
+                                  imagePath: Levels.imagePath2,
+                                  description: Levels.description2,
+                                );
+                              },
+                            );
+
+                            }
+                            if (Userlevel==3){
+                              showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return CustomAlert(
+                                  title: Levels.title3,
+                                  imagePath: Levels.imagePath3,
+                                  description: Levels.description3,
+                                );
+                              },
+                            );
+
+                            }
+                          },
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        ListTile(
+                          title: const Text('Your Achievements'),
+                          leading: const ImageIcon(
+                            AssetImage("assets/images/medal.png"),
+                            color: Palette.black,
+                            size: 30,
+                          ),
+                          trailing: Icon(Icons.arrow_forward),
+                          tileColor: Palette.mainColorShade,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                      ],
                     ),
                   ),
-                  _imageProfile(),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ListTile(
-                title: const Text('Your Level'),
-                subtitle: const Text('Level name'),
-                leading: const ImageIcon(
-                  AssetImage("assets/images/level-up.png"),
-                  color: Palette.black,
-                  size: 30,
                 ),
-                trailing: Icon(Icons.arrow_forward),
-                tileColor: Palette.mainColorShade,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                onTap: (){
-                  if (Userlevel==1){
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return CustomAlert(
-                          title: Levels.title1,
-                          imagePath: Levels.imagePath1,
-                          description: Levels.description1,
-                        );
-                      },
-                    );
-                  } 
-                  if (Userlevel==2){
-                    showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return CustomAlert(
-                        title: Levels.title2,
-                        imagePath: Levels.imagePath2,
-                        description: Levels.description2,
-                      );
-                    },
-                  );
-
-                  }
-                  if (Userlevel==3){
-                    showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return CustomAlert(
-                        title: Levels.title3,
-                        imagePath: Levels.imagePath3,
-                        description: Levels.description3,
-                      );
-                    },
-                  );
-
-                  }
-                },
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ListTile(
-                title: const Text('Your Achievements'),
-                leading: const ImageIcon(
-                  AssetImage("assets/images/medal.png"),
-                  color: Palette.black,
-                  size: 30,
-                ),
-                trailing: Icon(Icons.arrow_forward),
-                tileColor: Palette.mainColorShade,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+              );
+            }else {
+              return CircularProgressIndicator();
+            }
+          }
+      );
+    });
   }
+
+
+
+  
+  
 
   Widget _imageProfile() {
     return Stack(children: <Widget>[
